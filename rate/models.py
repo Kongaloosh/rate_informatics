71dec6e96fecb324bd3f96a0eef3e681e660d92e
Alex <konga.kearney@gmail.com>
2015-07-11 18:50:14 -0600

    made models

--- rate/models.py
+++ rate/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Lecturer(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.DateTimeField('date published')
+
+
+class Class(models.Model):
+    initials = models.CharField()
+    title = models.CharField()
+
+
+class Rating(models.Model):
+    year = models.IntegerField()
+    month = models.IntegerField()
+    day = models.IntegerField()
+    lecturers = models.ManyToManyField(Lecturer)
